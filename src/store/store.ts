8a919b1ae9d9d4ccb4a5f032f2c035aaa93c02f2
akashsaha02 <asaha8313@gmail.com>
2025-08-27 08:48:52 +0600

    updated code for dynamic tab

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,13 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./Slices/counterSlice/counterSlice";
 import authReducer from "./Slices/AuthSlice/authSlice";
 import formReducer from "./Slices/FormSlice/FormSlice";
-// import languageReducer from "./Slices/LanguageSlice/languageSlice";
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     auth: authReducer,
     form: formReducer,
-    // language: languageReducer,
   },
 });
 
