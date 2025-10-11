39d66fa0e8b0d49896cd4cf2c895071ce47aba8b
akashsaha02 <asaha8313@gmail.com>
2025-10-11 16:03:09 +0600

    updated video and podcast get

--- src/app/(HomeRoute)/podcast-video/Podcasts.tsx
+++ src/app/(HomeRoute)/podcast-video/Podcasts.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import FeatureVideoCard from "@/components/live-podcast/FeatureVideoCard";
 import PrimaryHeading from "@/components/reusable/PrimaryHeading";
-import { useGetAllVideosQuery } from "@/store/features/videoPodcast/video.api";
+import { useGetAllPodcastsQuery } from "@/store/features/videoPodcast/podcast.api";
 
-const WeeklyMagazine = () => {
-  const { data, isLoading, isError } = useGetAllVideosQuery({});
+const Podcasts = () => {
+  const { data, isLoading, isError } = useGetAllPodcastsQuery({});
 
   if (isLoading) return <p className="text-center text-gray-500">Loading...</p>;
   if (isError)
-    return <p className="text-center text-red-500">Error loading videos.</p>;
+    return <p className="text-center text-red-500">Error loading podcasts.</p>;
 
   const videos = data?.data || [];
 
   return (
     <div>
-      <PrimaryHeading title="Featured Video Articles" className="mb-6" />
+      <PrimaryHeading title="Featured Podcasts" className="mb-6" />
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {videos.map((item: any) => (
@@ -38,4 +38,4 @@ const WeeklyMagazine = () => {
   );
 };
 
-export default WeeklyMagazine;
+export default Podcasts;
